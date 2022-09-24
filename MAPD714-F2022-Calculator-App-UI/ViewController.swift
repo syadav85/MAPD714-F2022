7dac41e6b98f995204268789e9fa457906d1da8e
Satender Yadav <satenderyadav@Sams-Machine.local>
2022-09-24 14:29:00 -0400

    Added calculator UI

---
+++ MAPD714-F2022-Calculator-App-UI/ViewController.swift
@@ -0,0 +1,21 @@
+/*
+  File name: ViewController.swift
+  App description : Calculator App
+  Author's name: Satender Yadav, Apeksha Parmar
+  StudentID: 301293305,301205325
+  Date: 9/23/22.
+  Version: 1.0
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
